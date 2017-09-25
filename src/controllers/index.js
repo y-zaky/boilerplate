89f61d5f3f4e97dcbeec16f8d826a43ae5ff1520
Yahia <zaky20@hotmail.com>
2017-09-25 12:41:55 +0100

    server is running and works

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -2,14 +2,12 @@ const express = require('express');
 const path = require('path');
 const router = express.Router();
 
-// import home route controller - add controllers here 
+// import home route controller - add controllers here
 const home = require('./home');
 const error = require('./error');
 
 // add home route
 router.get('/', home.get);
-router.get('/fruits', fruits.get);
-router.get('/fruits/:fruit', fruit.get);
 router.use(error.client);
 router.use(error.server);
 

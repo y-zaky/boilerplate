5ccc1fb276cae6da8188d1701c099f40ad915a81
Yahia <zaky20@hotmail.com>
2017-09-25 12:34:07 +0100

    added errors and other controllers

---
+++ src/controllers/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const router = express.Router();
+
+// import home route controller - add controllers here 
+const home = require('./home');
+const error = require('./error');
+
+// add home route
+router.get('/', home.get);
+router.get('/fruits', fruits.get);
+router.get('/fruits/:fruit', fruit.get);
+router.use(error.client);
+router.use(error.server);
+
+module.exports = router;
